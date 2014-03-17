7837bd777c2e542b8061f58c6f05274ee7d20132
yg.jang <yongkyu.jang@gmail.com>
2014-03-17 19:00:50 +0900

    Commit Test

--- SC_HTTP_TEST/src/com/ibm/swg/sterling/demo/api/AdjustInventory.java
+++ SC_HTTP_TEST/src/com/ibm/swg/sterling/demo/api/AdjustInventory.java
@@ -6,6 +6,12 @@ import java.util.ArrayList;
 import com.ibm.klab.util.FileContentReader;
 import com.ibm.swg.sterling.demo.SterlingAPIWrapper;
 
+
+
+
+
+
+
 public class AdjustInventory {
 	private static final String templateFile = "adjustInventory.template.xml";
 	
