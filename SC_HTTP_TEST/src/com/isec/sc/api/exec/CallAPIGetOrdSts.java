1930420ab7d6388f8a4122402817b96a27ddb292
yg.jang <yongkyu.jang@gmail.com>
2014-03-18 16:56:23 +0900

    신규등록

---
+++ SC_HTTP_TEST/src/com/isec/sc/api/exec/CallAPIGetOrdSts.java
@@ -0,0 +1,33 @@
+package com.isec.sc.api.exec;
+
+import java.io.UnsupportedEncodingException;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+
+import com.isec.sc.api.cm.ManageItem;
+import com.isec.sc.api.core.util.FileContentReader;
+import com.isec.sc.api.inv.GetInventorySupply;
+import com.isec.sc.api.omp.CreateOrder;
+import com.isec.sc.api.omp.GetOrderStatus;
+
+public class CallAPIGetOrdSts {
+
+	/**
+	 * @param args
+	 * @throws UnsupportedEncodingException 
+	 */
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		
+		
+		// Order Status 조회
+		GetOrderStatus os = new GetOrderStatus();
+		os.setEnterpriseCd("Matrix");
+		os.setOrderNo("ORDER_TEST_3");
+		String status = os.run();
+		
+		System.out.println("Order Status: " + status);
+		
+	}
+
+}
+
